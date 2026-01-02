4bf3110bc7bd6da4f40bb14df72fb7454e0561a0
Thitna Damtew <thitnadamtew@gmail.com>
2026-01-02 18:55:26 +0300

    Update fetch URL for prediction API

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ form.addEventListener("submit", async (e) => {
   }
 
   try {
-    const response = await fetch("https://mall-customer-api-1.onrender.com/predict", {
+    const response = await fetch("https://mall-customer-api-1.onrender.com", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
