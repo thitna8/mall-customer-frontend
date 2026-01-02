65e45a49fb611e240cbc6ec88fd6e212d0188029
Thitna Damtew <thitnadamtew@gmail.com>
2026-01-02 19:12:50 +0300

    Update API endpoint for fetch request

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ form.addEventListener("submit", async (e) => {
   }
 
   try {
-    const response = await fetch("https://mall-customer-api-1.onrender.com", {
+    const response = await fetch("https://mall-customer-api-1.onrender.com/predict", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
